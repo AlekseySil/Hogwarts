02fa0ce0e82f5d6be84a8da835c23c2e62cf28e5
Aleksey909 <asilkin250@gmail.com>
2023-05-01 00:53:00 +0300

    homework done

---
+++ src/Hufflepuff.java
@@ -0,0 +1,36 @@
+public class Hufflepuff extends Hogwarts{
+    private int industriousness;
+    private int loyalty;
+    private int honour;
+
+    public Hufflepuff(String name, int magic, int transgression, int industriousness, int loyalty, int honour){
+        super(name, magic, transgression);
+        this.industriousness = industriousness;
+        this.loyalty = loyalty;
+        this.honour = honour;
+    }
+
+    public int getIndustriousness() {
+        return industriousness;
+    }
+
+    public void setIndustriousness(int industriousness) {
+        this.industriousness = industriousness;
+    }
+
+    public int getLoyalty() {
+        return loyalty;
+    }
+
+    public void setLoyalty(int loyalty) {
+        this.loyalty = loyalty;
+    }
+
+    public int getHonour() {
+        return honour;
+    }
+
+    public void setHonour(int honour) {
+        this.honour = honour;
+    }
+}
